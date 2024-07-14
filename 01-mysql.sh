c9194065b1a22af0856a00481f7d57e8211cb24d
Akhireddy <akhir1989@gmail.com>
2024-07-14 08:33:02 +0530

    mysql

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo "hello good morning"
+timestamp=$(date +%F-%H-%M-%S)
+script-name=$( echo $0 | cut -d "." -f1)
+logfile=/tmp/$script-name-timestamp.log 
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then 
+    echo "run with a root user"
+    exit 1
+else 
+    echo "you are a root user"
+fi
+
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is failure" &>>$logfile
+        exit 1 
+    else 
+         echo "$2 is success" &>>$logfile
+    fi      
+}         
+
+dnf -y install mysql &>>$logfile
+validate $? "installing mysql"
