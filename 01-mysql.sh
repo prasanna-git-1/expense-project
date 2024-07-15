294ae605b474ca4a86376630875d0189290d7e5a
Akhireddy <akhir1989@gmail.com>
2024-07-15 20:01:52 +0530

    backend server

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -45,7 +45,7 @@ validate $? "starting mysqld...."
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
 # validate $? "password setup "
 
-mysql -h 3.95.180.191 -uroot ExpenseApp@1 -e "show databases"; &>>$logfile
+mysql -h ip -uroot ExpenseApp@1 -e "show databases"; &>>$logfile
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root--pass ExpenseApp@1 &>>$logfile
