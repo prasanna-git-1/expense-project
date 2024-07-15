3c284ca3d3cc2651c4b5ef0827aaa5c017c16985
Akhireddy <akhir1989@gmail.com>
2024-07-15 08:17:33 +0530

    removed log file

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -13,10 +13,10 @@ N="\e[0m"
 validate(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2 .. $R failure $N" 
+        echo -e "$2 .. $R failure $N" 
         exit 1 
     else 
-         echo "$2 ..$G success $N" 
+         echo -e "$2 ..$G success $N" 
     fi      
 }         
 
